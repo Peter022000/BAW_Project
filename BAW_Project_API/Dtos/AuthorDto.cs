10dff517b86517a098bf60cb5d64a5d6d7737811
Piotr Duda <piotr.duda00@gmail.com>
2025-01-16 17:09:41 +0100

    implement authentication and authorisation

--- BAW_Project_API/Dtos/AuthorDto.cs
+++ BAW_Project_API/Dtos/AuthorDto.cs
@@ -2,7 +2,7 @@
 {
     public class AuthorDto
     {
-        public string FirstName { get; set; } = string.Empty;
-        public string LastName { get; set; } = string.Empty;
+        public required string FirstName { get; set; } = string.Empty;
+        public required string LastName { get; set; } = string.Empty;
     }
 }
